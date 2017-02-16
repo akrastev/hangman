93187405dc66fb2e5d1f87603723f91abdba52ba
Asen Krastev <asenkrastev@hotmail.com>
2017-02-16 23:57:21 +0200

    Add the Hanged picture and guessing
    - Add the consequentive states of a hanging as ASCII art pictures.
    - Add basic input of a single character.

--- hangman/source/app.d
+++ hangman/source/app.d
@@ -4,10 +4,56 @@ import std.stdio;
 /// This is our DB layer.
 string[] words = [ "how", "now", "brown", "cow" ];
 
+/// The hanged man. A picture gallery.
+string[] hangedMan = [
+	"",
+
+	"|\n" ~
+	"|\n" ~
+	"|\n",
+
+	"|- - |\n" ~
+	"|\n" ~
+	"|\n",
+
+	"|- - |\n" ~
+	"|    |\n" ~
+	"|\n",
+
+	"|- - |\n" ~
+	"|    |\n" ~
+	"|   / \\\n" ~
+	"|   \\ /\n",
+
+	"|- - |\n" ~
+	"|    |\n" ~
+	"|   / \\\n" ~
+	"|   \\ /\n" ~
+	"|    |\n" ~
+	"|\n",
+];
 
 /// The game itself.
 void main()
 {
 	auto index = uniform(0, words.length);
 	writeln("Chosen word: ", words[index]);
+
+	immutable auto errors = 5;
+	writeln("Hanging:\n", hangedMan[errors]);
+
+	char[] guess;
+	write("Make a guess: ");
+	readln(guess);
+
+	assert(guess.length > 0);
+
+	const char ch = guess[0];
+	if (guess.length > 1)
+	{
+		writeln("The guess contained more than one character.\n" ~
+		        "Taking the first character as the actual guess.");
+	}
+
+	writeln("Guess: ", ch);
 }
