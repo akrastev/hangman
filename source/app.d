243f9021e9bb9d071c9375608aba3ad5f4136a25
Asen Krastev <akrastev@vmware.com>
2017-02-17 21:00:41 +0200

    Add getting a random word from a service
    Do a simple HTTP Get on a random-word-as-a-service URL and get the random word. Voila!

--- source/app.d
+++ source/app.d
@@ -2,6 +2,7 @@ import std.random;
 import std.range;
 import std.stdio;
 import vibe.d;
+import vibe.http.client;
 
 /// This is our DB layer.
 string[] words = [ "how", "now", "brown", "cow" ];
@@ -42,15 +43,29 @@ void main()
 	auto word = words[index];
 	writeln("Chosen word: ", word);
 
+	string randomWord = "I hope something random comes through HTTP.";
+
+	requestHTTP("http://www.setgetgo.com/randomword/get.php",
+		(scope req) {
+			req.method = HTTPMethod.GET;
+		},
+		(scope res) {
+			randomWord = res.bodyReader.readAllUTF8();
+		}
+	);
+
+	writeln("Random word: ", randomWord);
+
 	auto errors = 0;
 	writeln("Hanging:\n", hangedMan[errors]);
 
     auto wordStat = repeat("_", word.length).join(' ');
 	writeln("Guess the word: ",  wordStat);
 
-	char[] guess;
+	//char[] guess = "guess";
+	const char[] guess = "boo";
 	write("Guess a letter: ");
-	readln(guess);
+	// readln(guess);
 
 	assert(guess.length > 0);
 	const char ch = guess[0];
